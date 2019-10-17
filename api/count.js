136ba31732c1defd35b927dbd4a6c439563d9a24
huchengyong <1437136500@qq.com>
2019-10-17 20:38:01 +0800

    refix result of count function  with group function

--- api/count.js
+++ api/count.js
@@ -2,10 +2,16 @@ const query = require('./query')
 
 module.exports = (mysql, field) => {
     field = (field && field !== '*') ? '`' + field.split('.').join('`.`') + '`' : '*'
-    mysql.sql = 'SELECT COUNT(' + field + ') AS res FROM ' + '`' + mysql.tableName + '`' + mysql.aliasStr
+    field = mysql.groups ? field : "COUNT(" + field + ") AS db_count"
+    mysql.sql = 'SELECT ' + field + ' FROM ' + '`' + mysql.tableName + '`' + mysql.aliasStr
         + mysql.joinStr
-        + mysql.getWheres()
+        + mysql.getWheres() + ' ' + mysql.groups + ' '
+        + mysql.getOrders()
+        + mysql.getLimits()
+    if (mysql.groups) {
+        mysql.sql = 'SELECT COUNT(' + field + ') as db_count FROM (' + mysql.sql + ')e'
+    }
     return query(mysql, mysql.sql).then((data) => {
-        return data.length ? data[0].res : 0
+        return data.length ? data[0].db_count : 0
     })
 }
